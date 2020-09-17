e689a86b7c33407df658cb102818727940b6e210
Yair <yairshop@gmail.com>
2020-09-17 10:52:44 -0400

    Add debug mode

--- driver.py
+++ driver.py
@@ -1,26 +1,42 @@
 import smtplib, ssl
 import argparse
 import pandas as pd
+from os import system
+from smtpd import DebuggingServer
+import asyncore
+import threading
+from time import sleep
+
+def run_debug_server():
+	server = DebuggingServer(('localhost', 1025), None)
+	asyncore.loop()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--password", action="store", required=True)
-parser.add_argument("--data", action="store", required=True)
 parser.add_argument("--message", action="store", required=True)
+parser.add_argument("--data", action="store", required=True)
 parser.add_argument("--sender", action="store", required=True)
+parser.add_argument("--password", action="store", required=True)
+parser.add_argument("--no-debug", action="store_true")
 args = parser.parse_args()
 
-smtp_server = "smtp.gmail.com"
-port = 465
+smtp_server = "smtp.gmail.com" if args.no_debug else "localhost"
+port = 465 if args.no_debug else 1025
 password = args.password
 sender_email = args.sender
 
 data = pd.read_csv(args.data, dtype=str)
 msg = open(args.message).read()
 
-context = ssl.create_default_context()
+if args.no_debug:
+	context = ssl.create_default_context()
+else:
+	th = threading.Thread(target=run_debug_server)
+	th.start()
+	sleep(1)
 
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-	server.login(sender_email, password)
+with smtplib.SMTP_SSL(smtp_server, port, context=context) if args.no_debug else smtplib.SMTP(smtp_server, port) as server:
+	if args.no_debug:
+		server.login(sender_email, password)
 
 	locations = {
 		"1": "Chapel Fields",
@@ -42,6 +58,9 @@ with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
 		row["Maariv 3"])
 
 		server.sendmail(sender_email, receiver_email, body)
+
+if not args.no_debug:
+	th.join()
 
 # shtick_signature = """\
 # Daniella Bloch, Gabbait
